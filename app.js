0bbf0e99f318524caac3cc712d0cbf54b5d4e40a
Bucko Akos <akosbucko@gmail.com>
2020-05-08 09:23:05 +0200

    final calculator

--- app.js
+++ app.js
@@ -1,1 +1,17 @@
-console.log('A számológép használatra kész.');
+function calculateExcess() {
+
+  var totalDistance = prompt('Enter the total distance value in kilometers.');
+
+  var averageConsumption = prompt('Enter the average fuel consumption value over 100 kilometers.');
+
+  var tankCapacity = prompt('Enter the fueltank capacity value of the space shuttle.');
+  
+  var requiredFuel = totalDistance * (averageConsumption / 100);
+  
+  var excessFuel = tankCapacity - requiredFuel;
+  
+  alert('The required amount of fuel is: ' + requiredFuel + ' liters. \n The amount of fuel to be drained is: ' + excessFuel + ' liters.');
+
+}
+
+calculateExcess();
